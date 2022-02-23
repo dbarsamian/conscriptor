fae6f5354f60c4b809d94052ac75b1a91dd82adb
David Barsamian <david@davidbarsam.com>
2022-02-22 21:39:33 -0800

    Improved format function

--- Conscriptor/Controllers/Markdown/MarkdownEditorController.swift
+++ Conscriptor/Controllers/Markdown/MarkdownEditorController.swift
@@ -9,38 +9,34 @@ import AppKit
 import Foundation
 
 struct MarkdownEditorController {
+    /// Iterates over all selected ranges in a `textView`, applying or removing a `style` if found or not found.
+    /// Within a selection, if text is found to contain any Markdown syntax defined in `MarkdownFormatter.Formatting`,
+    /// then the text will be replaced in the `textView` without the syntax. The opposite is true for text not containing any
+    /// Markdown syntax.
+    ///
+    /// Example: With a selection "`**Text**`" and a style of `.bold`, the function will modify the `textView` to replace
+    /// the selection with "`Text`".
     public static func format(_ document: inout ConscriptorDocument,
                               with style: MarkdownFormatter.Formatting,
                               in textView: NSTextView?) {
         guard let textView = textView else {
             return
         }
-        // NSValue result is equal to the start position of the selected range, and how long it is in characters.
         let ranges = textView.selectedRanges.map { $0.rangeValue }
         for range in ranges {
-            // Use rawValue.length to get expanded range based on syntax operator size
-            // Ex. Bold syntax ** is longer than italic syntax *
-            let expandedRange = NSRange(location: range.lowerBound - 2, length: range.lowerBound + range.upperBound)
+            let expandedRange = NSRange(location: range.location - style.rawValue.length,
+                                        length: range.length + (style.rawValue.length * 2))
             let selection = document.text[Range(range)!]
             let expandedSelection = document.text[Range(expandedRange)!]
 
-            // If the expanded range contains formatting
-            // replace expanded subrange with style
-            // Else replace the regular range with style
             if MarkdownFormatter.isFormatted(expandedSelection, as: style) {
-                let startIndex = document.text.index(document.text.startIndex, offsetBy: expandedRange.lowerBound)
-                let endIndex = document.text.index(document.text.startIndex, offsetBy: expandedRange.upperBound)
-                let nsRange = NSRange(startIndex ..< endIndex, in: document.text)
                 let replacement = MarkdownFormatter.format(expandedSelection, style: style)
-                textView.insertText(replacement, replacementRange: nsRange)
+                textView.insertText(replacement, replacementRange: expandedRange)
             } else {
-                let startIndex = document.text.index(document.text.startIndex, offsetBy: range.lowerBound)
-                let endIndex = document.text.index(document.text.startIndex, offsetBy: range.upperBound)
-                let nsRange = NSRange(startIndex ..< endIndex, in: document.text)
                 let replacement = MarkdownFormatter.format(selection, style: style)
-                textView.insertText(replacement, replacementRange: nsRange)
+                textView.insertText(replacement, replacementRange: range)
             }
-            document.text = textView.string
         }
+        document.text = textView.string
     }
 }
